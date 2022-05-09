3bc9726ae3a4133c1de69625ed1a4bf004b8e527
Mikemece <mcmiguel313@gmail.com>
2022-05-10 01:43:40 +0200

    MONEY UPDATE

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const Inventario = document.getElementById("inventario")
+
 const Pinwi = document.getElementById("pinwi")
 const Name = document.getElementById("name")
 const Head = document.getElementById("head")
@@ -6,30 +6,40 @@ const body = document.getElementById("body")
 const d1 = document.getElementById("d1")
 const d2 = document.getElementById("d2")
 const Ropa = document.getElementById("roro")
+const money = document.getElementById("money")
 
-
-// const Name = document.getElementById("name")
-
-
-const pinwiRect = Pinwi.getBoundingClientRect()
-const nameRect = Name.getBoundingClientRect()
-
-// var clicked = 0
-// var pinwiclicked = 0
 var exp = 0
 var lvl = 0
 var initWidth = Pinwi.clientWidth
+var mon = 0
+var cont=0
 
 Pinwi.addEventListener("click", pinwiFunction) 
 d2.addEventListener("click", pinwiFunction) 
 Ropa.addEventListener("click", dress) 
+money.addEventListener("click", hereComes) 
+
+
+function hereComes(){
+    mon++
+    money.innerHTML=mon +"€"
+}
+
 
 function dress(){
-    Head.src="./ropa/gorroF.png";
-    body.src="./ropa/pajaF.png";
-    d1.src="./ropa/amogusF.png";
-    d2.src="./ropa/gatoF.png";
+    if(cont==0){
+        Head.src="./ropa/gorroF.png";
+        body.src="./ropa/cadenaF.png";
+        d2.src="./ropa/gatoF.png";
+        cont++
+    }else{
+        Head.src="./ropa/narutoF.png";
+        body.src="./ropa/betisF.png";
+        d2.src="./ropa/amogusF.png";
+        cont--
+    }
 }
+
 
 function pinwiFunction () {
     // console.log(pinwiclicked)
@@ -62,26 +72,11 @@ function pinwiFunction () {
     setTimeout(() => {  body.style.width = initWidth + "px"; }, 150);
     setTimeout(() => {  d1.style.width = initWidth + "px"; }, 150);
     setTimeout(() => {  d2.style.width = initWidth + "px"; }, 150);
-    
-    if(lvl<4) {
-        if(pinwiRect.y <= nameRect.y+nameRect.height){
-            console.log("Collision!")
-            Name.style.top = (Name.offsetTop - 1) + "px"
-        }
-
-        
-    }
 
     console.log(nameRect.top)
-    Lvl.innerHTML = "Nivel "+ lvl
+    Lvl.innerHTML = "LVL "+ lvl
     Exp.innerHTML = exp + " EXP"
 
-    // if(!pinwiclicked) {
-    //     Name.style.animation = "sizing 1s ease-in-out 0s infinite alternate"
-    //     pinwiclicked=1
-    // }else{
-    //     pinwiclicked=0
-    // } 
     }
 
 
